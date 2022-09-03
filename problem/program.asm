5faa47e497779b91730039e5b676df1ce1ebf658
Juan Arango <68034390+jugabriel77@users.noreply.github.com>
2022-09-03 16:49:31 -0500

    Evaluación Funcionando

--- problem/program.asm
+++ problem/program.asm
@@ -1,4 +1,112 @@
-// Juan Gabriel Arango Perez
+// JUAN GABRIEL ARANGO PEREZ
 // 000402035
-// juan.arangop@upb.edu.co
+// JUAN.ARANGOP@UPB.EDU.CO
 
+
+(INICIO) 
+ @KBD
+ D=M
+ @67		//DIRECCION DE C
+ D=D-A 
+ @BORRAR
+ D;JEQ
+ @KBD
+ D=M
+ @84		//DIRECCION DE T
+ D=D-A 
+ @CRUZ
+ D;JEQ
+ @INICIO
+ 0;JMP
+ 
+(BORRAR)
+ @16384                 //16384 + (128*32)
+ D=A
+ @IDIB                 //INICIO DEL DIBUJO
+ M=D 
+ @8192                  //CANTIDAD DE REGISTROS A PINTAR
+ D=A
+ @CONTADOR                  //CONTADOR PARA LAS FILAS
+ M=D
+ @1
+ D=A 
+ @SALTAR
+ M=D
+ @COLOR
+ M=0
+ @DIR1
+ 0;JMP
+
+ (CRUZ)
+ @16400 
+ D=A
+ @IDIB 
+ M=D 
+ @256 
+ D=A
+ @CONTADOR 
+ M=D
+ @32
+ D=A 
+ @SALTAR
+ M=D
+ @COLOR
+ M=1
+ @BOOLEANO
+ M=0
+ @DIR1
+ 0;JMP
+
+ (CRUZ2)
+ @20480 
+ D=A
+ @IDIB 
+ M=D 
+ @32 
+ D=A
+ @CONTADOR 
+ M=D
+ @1
+ D=A 
+ @SALTAR
+ M=D
+ @COLOR
+ M=-1
+ @BOOLEANO
+ M=1
+ @DIR1
+ 0;JMP
+
+(DIR1)
+ @IDIB 
+ D=M
+ @pscreen
+ M=D 
+ 
+(PINTAR)
+ @CONTADOR 
+ D=M 
+ @END
+ D;JEQ 
+ @COLOR
+ D=M 
+ @pscreen
+ A=M 
+ M=D 
+ @CONTADOR 
+ M=M-1
+ @SALTAR
+ D=M
+ @pscreen
+ M=M+D 
+ @PINTAR
+ 0;JMP
+
+(END)
+ @BOOLEANO
+ D=M 
+ @CRUZ2
+ D;JEQ
+
+ @INICIO
+ 0;JMP
